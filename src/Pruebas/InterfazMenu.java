1791062addc452eeec3f587360ca6de86ff46939
PabloMateosP <pmatpal0105@g.educaand.es>
2023-05-27 12:13:10 +0200

    Practica 9 subida

--- src/Pruebas/InterfazMenu.java
+++ src/Pruebas/InterfazMenu.java
@@ -12,7 +12,8 @@ public class InterfazMenu {
     public static void main(String[] args) {
         InterfazMenu a = new InterfazMenu();
     }
-    private InterfazMenu(){
+
+    private InterfazMenu() {
         //Creación frame para principal
         Frame ventaPrincipal = new Frame("Intervenciones Alumnos");
 
@@ -26,7 +27,7 @@ public class InterfazMenu {
         menu.add(item1);
         barraMenu.add(menu);
         ventaPrincipal.setMenuBar(barraMenu);
-        ventaPrincipal.setSize(800,800);
+        ventaPrincipal.setSize(800, 800);
         ventaPrincipal.setLayout(null);
         ventaPrincipal.setVisible(true);
         ventaPrincipal.setBackground(new Color(0xBABA96));
@@ -34,7 +35,7 @@ public class InterfazMenu {
         //Texto bienvenido
         Label textoBienvenida = new Label("Bienvenido al programa para la modificacion de una base de datos");
         textoBienvenida.setVisible(true);
-        textoBienvenida.setBounds(150,50,400,25);
+        textoBienvenida.setBounds(150, 50, 400, 25);
         textoBienvenida.setBackground(new Color(0xBABA96));
         textoBienvenida.setForeground(new Color(000000));
         ventaPrincipal.add(textoBienvenida);
@@ -42,7 +43,7 @@ public class InterfazMenu {
         //Creamos label para barra estética
         Label textoART = new Label("|------------------------------------------------------------------------------------------------|");
         textoART.setVisible(true);
-        textoART.setBounds(142,75,400,10);
+        textoART.setBounds(142, 75, 400, 10);
         textoART.setBackground(new Color(0xBABA96));
         textoART.setForeground(new Color(000000));
         ventaPrincipal.add(textoART);
@@ -56,33 +57,71 @@ public class InterfazMenu {
 
         //Frame borrarBase
         Frame frameBorrarBase = new Frame("Borrar Base");
-        frameBorrarBase.setSize(400,400);
+        frameBorrarBase.setSize(400, 400);
         frameBorrarBase.setLayout(null);
-        Label labeltextoBorrarBase = new Label("Escriba la ruta de su archivo:");
-        labeltextoBorrarBase.setBounds(10,20,160,100);
-        labeltextoBorrarBase.setForeground(Color.BLACK);
-        frameBorrarBase.add(labeltextoBorrarBase);
+        Label labeltextoRutaBase = new Label("Escriba la ruta de su archivo:");
+        labeltextoRutaBase.setBounds(10, 20, 160, 100);
+        labeltextoRutaBase.setForeground(Color.BLACK);
+        frameBorrarBase.add(labeltextoRutaBase);
+
+
+        Label labeltextoNombreBase = new Label("Escriba el nombre de su base:");
+        labeltextoNombreBase.setBounds(10, 100, 170, 100);
+        labeltextoNombreBase.setForeground(Color.BLACK);
+        frameBorrarBase.add(labeltextoNombreBase);
+
         //Todo: introducir en los métodos una variable string la cual sea introducida mediante el Textfield como
-        // por ejemplo la ruta en esete caso de la base de datos.
+        // por ejemplo la ruta en este caso de la base de datos.
         TextField textoBorrarBase = new TextField();
-        textoBorrarBase.setBounds(175, 60, 100,20);
+        textoBorrarBase.setBounds(175, 60, 100, 20);
         frameBorrarBase.add(textoBorrarBase);
+
+        TextField textoNombreBase = new TextField();
+        textoNombreBase.setBounds(190, 140, 100, 20);
+        frameBorrarBase.add(textoNombreBase);
+
+        Button aceptar = new Button("Aceptar");
+        aceptar.setBounds(150, 300, 50, 50);
+        frameBorrarBase.add(aceptar);
+
 
 
         botonBorraBase.addActionListener(new ActionListener() {
+
             public void actionPerformed(ActionEvent e) {
                 frameBorrarBase.setVisible(true);
-                labeltextoBorrarBase.setVisible(true);
+                labeltextoNombreBase.setVisible(true);
                 textoBorrarBase.setVisible(true);
+                textoNombreBase.setVisible(true);
+                labeltextoRutaBase.setVisible(true);
+                labeltextoRutaBase.setVisible(true);
+                aceptar.setVisible(true);
+
+                textoNombreBase.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        String rutaBase = textoBorrarBase.getText();
+                        String nombreBase = textoNombreBase.getText();
+                        aceptar.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                CrearDatabaseINTERFAZ.CrearDatabase(rutaBase, nombreBase);
+                            }
+                        });
+                    }
+
+                });
+
+                frameBorrarBase.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e) {
+                        frameBorrarBase.dispose();
+                    }
+                });
             }
         });
         //Cerrar frame borrar base mediante cruz roja
-        frameBorrarBase.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                frameBorrarBase.dispose();
-            }
-        });
+
         ventaPrincipal.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
